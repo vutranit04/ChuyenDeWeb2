90e5a7268e6c0d674f61a8e82a9dbb158aa42051
TranMinhVu <vutranit04@gmail.com>
2026-06-03 18:37:47 +0700

    New project, new hellocontroller test

---
+++ src/main/java/com/minhvu/spring_demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.minhvu.spring_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Spring Boot! This is Tran Minh Vu";
+    }
+}
